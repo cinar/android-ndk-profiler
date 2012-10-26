9cdf1536ca571c9f79b2d1844a584bf32f998f18
Onur Cinar <onur.cinar@gmail.com>
2012-10-25 23:15:44 -0700

    Converting the Android NDK Profiler to a NDK native module that can be used under sources through import-module keyword.

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,4 +4,13 @@ include $(CLEAR_VARS)
 LOCAL_CFLAGS := -fvisibility=hidden
 LOCAL_MODULE    := andprof
 LOCAL_SRC_FILES := gnu_mcount.S prof.c read_maps.c
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+
+LOCAL_EXPORT_CFLAGS += -pg
+LOCAL_EXPORT_CFLAGS += -fno-function-sections
+LOCAL_EXPORT_CFLAGS += -fno-omit-frame-pointer
+
+LOCAL_EXPORT_LDLIBS += -llog
+
 include $(BUILD_STATIC_LIBRARY)
